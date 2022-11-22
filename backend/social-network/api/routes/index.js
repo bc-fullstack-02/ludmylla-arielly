dcdc954629054340d64a190af4eb5aadaa517523
Ludmylla <ludmay4@gmail.com>
2022-11-22 18:11:51 -0300

    exporta as rotas

--- backend/social-network/api/routes/index.js
+++ backend/social-network/api/routes/index.js
@@ -1,3 +1,6 @@
 exports.User = require('./user.js')
 exports.Post = require('./post.js')
 exports.Comment = require('./comment.js')
+exports.Profile = require('./profile.js')
+exports.Security = require('./security.js')
+exports.Feed = require('./feed.js')
