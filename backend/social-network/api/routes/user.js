4a3cba4adce3175e0c68f20779665e4e7fef1157
Ludmylla <ludmay4@gmail.com>
2022-11-22 18:11:00 -0300

    refatora router  user

--- backend/social-network/api/routes/user.js
+++ backend/social-network/api/routes/user.js
@@ -1,66 +1,41 @@
 const createError = require('http-errors')
 const express = require('express')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const router = express.Router()
 const { User, Connection } = require('../models')
-const TOKEN_SECRET = '2634d3209b728707236765918773edda'
 
 router
   .route('/')
   .all((req, res, next) => Promise.resolve()
     .then(() => Connection.then())
-    .then(() => {
-      next()
-    })
-    .catch(err => next(err)))
-
-  // Lista users
-  .get((req, res, next) => Promise.resolve()
-    .then(() => User.find({}))
-    .then((data) => res.status(200).json(data))
-    .catch(err => next(err)))
-
-  // cria usuario
-  .post((req, res, next) => Promise.resolve()
-    .then(() => bcrypt.hash(req.body.password, 10))
-    .then((passwordHashed) => new User({ ...req.body, password: passwordHashed }).save())
-    .then((data) => res.status(201).json(data))
-    .catch(err => next(err)))
-
-router
-  .route('/login')
-
-  .post((req, res, next) => Promise.resolve()
-    .then(() => User.findOne({ user: req.body.user }))
-    .then((user) => user ? bcrypt.compare(req.body.password, user.password) : next(createError(404)))
-    .then((passwordHashed) => passwordHashed ? jwt.sign(req.body.user, TOKEN_SECRET) : next(createError(401)))
-    .then((acessToken) => res.status(201).json({ acessToken }))
+    .then(() => { next() })
     .catch(err => next(err)))
 
 router
   .param('id', (req, res, next, id) => Promise.resolve()
     .then(() => Connection.then())
-    .then(() => {
-      next()
-    })
+    .then(() => next())
     .catch(err => next(err)))
-  .route('/:id')
 
+  .route('/me')
+
+  // Search user by id
   .get((req, res, next) => Promise.resolve()
-    .then(() => User.findById(req.params.id)) /* .populate({ path: 'comments' }) */
+    .then(() => User.findById(req.user.id, ['-password']))
     .then((data) => data ? res.status(200).json(data) : next(createError(404)))
     .catch(err => next(err)))
 
+  // update user
   .put((req, res, next) => Promise.resolve()
     .then(() => bcrypt.hash(req.body.password, 10))
-    .then((passwordHashed) => User.findByIdAndUpdate(req.params.id, { ...req.body, password: passwordHashed, runValidators: true }))
+    .then((passwordHashed) => User.findByIdAndUpdate(req.user.id, { ...req.body, password: passwordHashed, runValidators: true, new: true, select: '-password' }))
     .then((data) => res.status(200).json(data))
     .catch(err => next(err)))
 
+  // delete user
   .delete((req, res, next) => Promise.resolve()
-    .then(() => User.deleteOne({ _id: req.params.id }))
-    .then((data) => res.status(203).json(data))
+    .then(() => User.deleteOne({ _id: req.user.id }))
+    .then((data) => data ? res.status(203).json(data) : next(createError(404)))
     .catch(err => next(err)))
 
 module.exports = router
