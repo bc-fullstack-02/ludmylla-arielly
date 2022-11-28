4b0859985fd874244567273b1d9b28cf18fe82d4
Ludmylla <ludmay4@gmail.com>
2022-11-28 15:27:11 -0300

    adiciona like e mensageria com rabbitmq na comentário

--- backend/social-network/api/routes/comment.js
+++ backend/social-network/api/routes/comment.js
@@ -25,6 +25,7 @@ router
     .then((comment) => Post.findById(comment.post)
       .then(post => Object.assign(post, { comment: [...post.comments, comment._id] }))
       .then(post => Post.findByIdAndUpdate(comment.post, post))
+      .then(args => req.publish('comment', [args.profile], args))
       .then(() => comment)
     )
     .then((data) => res.status(201).json(data))
@@ -44,11 +45,13 @@ router
     .then((data) => data ? res.status(200).json(data) : next(createError(404)))
     .catch(err => next(err)))
 
+  // atualiza comentario
   .put((req, res, next) => Promise.resolve()
     .then(() => Comment.findByIdAndUpdate(req.params.id, req.body, { runValidators: true, new: true }))
     .then((data) => res.status(200).json(data))
     .catch(err => next(err)))
 
+  // deleta comentario
   .delete((req, res, next) => Promise.resolve()
     .then(() => Comment.deleteOne({ _id: req.params.id }))
     .then((data) => res.status(203).json(data))
@@ -62,8 +65,25 @@ router
 
   .route('/:postId/comments/:id/like')
 
+  // da like no comentario
   .post((req, res, next) => Promise.resolve()
-    .then(() => Comment.findOneAndUpdate({ _id: req.params.id }, { $push: { likes: req.user.profile._id } }))
+    .then(() => Comment.findOneAndUpdate({ _id: req.params.id }, { $push: { likes: req.user.profile._id } }, { new: true }))
+    .then(args => req.publish('comment-like', [args.profile], args))
+    .then((data) => res.status(200).json(data))
+    .catch(err => next(err)))
+
+router
+  .param('postId', (req, res, next, id) => Promise.resolve()
+    .then(() => Post.findById(id))
+    .then((post) => post ? next() : next(createError(404)))
+    .catch(err => next(err)))
+
+  .route('/:postId/comments/:id/unlike')
+
+  // remove like do comentario
+  // mongo pull and push
+  .post((req, res, next) => Promise.resolve()
+    .then(() => Comment.findOneAndUpdate({ _id: req.params.id }, { $pull: { likes: req.user.profile._id } }, { new: true }))
     .then((data) => res.status(200).json(data))
     .catch(err => next(err)))
 
