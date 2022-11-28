8683381209c18e4de4acda11b31d84526655e83d
Ludmylla <ludmay4@gmail.com>
2022-11-28 15:28:59 -0300

    atualiza profile

--- backend/social-network/api/routes/profile.js
+++ backend/social-network/api/routes/profile.js
@@ -37,8 +37,8 @@ router
   .route('/:id/follow')
 
   .post((req, res, next) => Promise.resolve()
-    .then(() => Profile.findOneAndUpdate({ id: req.params.id }, { $push: { followers: req.user.profile.id } }))
-    .then(() => Profile.findOneAndUpdate({ id: req.user.profile.id }, { $push: { following: req.params.id } }, { new: true }))
+    .then(() => Profile.findOneAndUpdate({ _id: req.params.id }, { $push: { followers: req.user.profile._id } }))
+    .then(() => Profile.findOneAndUpdate({ _id: req.user.profile._id }, { $push: { following: req.params.id } }, { new: true }))
     .then((data) => data ? res.status(200).json(data) : next(createError(404)))
     .catch(err => next(err)))
 
@@ -51,8 +51,8 @@ router
   .route('/:id/unfollow')
 
   .post((req, res, next) => Promise.resolve()
-    .then(() => Profile.findOneAndUpdate({ id: req.params.id }, { $pull: { followers: req.user.profile.id } }))
-    .then(() => Profile.findOneAndUpdate({ id: req.user.profile.id }, { $pull: { following: req.params.id } }, { new: true }))
+    .then(() => Profile.findOneAndUpdate({ _id: req.params.id }, { $pull: { followers: req.user.profile._id } }))
+    .then(() => Profile.findOneAndUpdate({ _id: req.user.profile._id }, { $pull: { following: req.params.id } }, { new: true }))
     .then((data) => data ? res.status(200).json(data) : next(createError(404)))
     .catch(err => next(err)))
 
