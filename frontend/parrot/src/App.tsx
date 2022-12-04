687349364c81efd05afb9c01f8360d6b67d0944a
Ludmylla <ludmay4@gmail.com>
2022-12-04 15:35:09 -0300

    refatora e limpa o projeto

--- frontend/parrot/src/App.tsx
+++ frontend/parrot/src/App.tsx
@@ -1,10 +1,11 @@
+import Login from './Pages/Login'
 import './index.css'
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Login />
     </div>
   )
 }
