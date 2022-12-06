7a1e3c8fd2f9f6b1ae47d2e55b38ab3f9d02f751
Ludmylla <ludmay4@gmail.com>
2022-12-06 11:29:52 -0300

    cria componente de cadastro

---
+++ frontend/parrot/src/Pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import AuthForm from "../../components/AuthForm";
+
+
+function SignUp() {
+    return (
+        <AuthForm 
+            formTitle="Faça o cadastro e comece a usar!" 
+            submitFormButtonText="Cadastrar" 
+            linkDescription="Já possui conta? Entre agora!" />
+    )
+}
+export default SignUp;
