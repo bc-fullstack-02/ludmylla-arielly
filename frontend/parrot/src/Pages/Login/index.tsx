8b0109ed84806c2f64ce3f2f146a7b425d2174e3
Ludmylla <ludmay4@gmail.com>
2022-12-06 13:53:36 -0300

    cria handle login

--- frontend/parrot/src/Pages/Login/index.tsx
+++ frontend/parrot/src/Pages/Login/index.tsx
@@ -1,10 +1,21 @@
 import AuthForm from "../../components/AuthForm";
+import api  from '../../services/api'
 
 function Login() {
+
+   async function handleLogin(user: string, password: string) {
+      const data = await api.post("/security/login", {
+            user,
+            password
+        });
+        console.log(data)
+    }
+
     return (
         <AuthForm 
             formTitle="Faça o login e comece a usar!" 
             submitFormButtonText="Entrar" 
+            submitFormButtonAction={handleLogin}
             linkDescription="Não possui conta? Crie uma agora!" 
             routeName="/signup" />
     )
