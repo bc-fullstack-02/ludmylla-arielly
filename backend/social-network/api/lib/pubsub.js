3c30bffcf3157c8ea7c5bc9ebf45d3b435461e28
Ludmylla <ludmay4@gmail.com>
2022-12-10 14:23:04 -0300

    refatora pubsub

--- backend/social-network/api/lib/pubsub.js
+++ backend/social-network/api/lib/pubsub.js
@@ -2,11 +2,10 @@ const Rascal = require('rascal')
 const defaultConfig = require('../config.json')
 
 defaultConfig.vhosts['/'].connection.url = process.env.AMQP_URL || defaultConfig.vhosts['/'].connection.url
-
-const publischer = Object.keys(defaultConfig.vhosts['/'].publications)[0]
-const consumer = Object.keys(defaultConfig.vhosts['/'].subscriptions)[0]
-
 const config = Rascal.withDefaultConfig(defaultConfig)
+
+const publisher = Object.keys(config.vhosts['/'].publications)[0]
+const consumer = Object.keys(config.vhosts['/'].subscriptions)[0]
 
 module.exports = {
   pub: (req, res, next) => Rascal.Broker.create(Rascal.withDefaultConfig(config), function (err, broker) {
@@ -17,7 +16,7 @@ module.exports = {
         payload: value,
         keys
       }
-      broker.publish(publischer, msg, function (err, publication) {
+      broker.publish(publisher, msg, function (err, publication) {
         if (err) reject(err)
         publication.on('error', reject)
         resolve(value)
