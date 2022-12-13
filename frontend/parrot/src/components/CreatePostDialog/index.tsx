0662dcaaa7cba6bc08d1455691d58f5d26398e06
Ludmylla <ludmay4@gmail.com>
2022-12-13 19:53:15 -0300

    remove componente background

--- frontend/parrot/src/components/CreatePostDialog/index.tsx
+++ frontend/parrot/src/components/CreatePostDialog/index.tsx
@@ -40,11 +40,17 @@ interface PostFormElement extends HTMLFormElement {
        
     }
 
+    function backgroundRed() {
+        const color = 'bg-red-400'
+    }
+
+
     return (
        <Dialog.Portal>
         <Dialog.Overlay className='bg-black/60 inset-0 fixed' />
         <Dialog.Content className='fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25'>
             <Dialog.Title className='text-2xl font-black'>Novo Post</Dialog.Title>
+
             <form onSubmit={handleSubmit} className='mt-8 flex flex-col gap-2'>
                 <div className='flex flex-col gap-2'>
                     <label htmlFor="title" className='font-semibold'>Título do Post</label>
