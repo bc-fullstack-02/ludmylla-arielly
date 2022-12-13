5adeeefaba075a388829592588dd9c162539b244
Ludmylla <ludmay4@gmail.com>
2022-12-13 19:54:43 -0300

    refatora home

--- frontend/parrot/src/Pages/Home/index.tsx
+++ frontend/parrot/src/Pages/Home/index.tsx
@@ -1,41 +1,13 @@
-import { useState } from 'react';
-import * as Dialog from '@radix-ui/react-dialog'
-
 import Menu from '../../components/Menu';
-import Text from '../../components/Text';
-import CreatePostButton from '../../components/CreatePostButton';
-import CreatePostDialog from '../../components/CreatePostDialog';
 import Feed from '../../components/Feed';
 
-import logo_menu from '../../assets/logo-menu.svg'
-
-
 function Home() {
-    const [open, setOpen ] = useState(false);
-    function closeDialog() {
-        setOpen(false);
-    }
-
     return (
         <div className="w-screen h-screen flex">
-            <div className="basis-1/6 border-r border-slate-400 ml-4 pt-4">
-                <div className='flex items-center ml-4'>
-                    <img src={logo_menu} alt="Logo" />
-                    <Text className='font-extrabold ml-4'>Penguin</Text>
-                </div>
-                
-                <Menu />
-                <div className='flex flex-col items-center'>
-                    <Dialog.Root open={open} onOpenChange={setOpen}>
-                        <CreatePostButton />
-                        <CreatePostDialog closeDialog={closeDialog} />
-                    </Dialog.Root>
-                </div>
-
-            </div>
-            <div className="basis-5/6 overflow-y-auto scroll-smooth">
-                <Feed />
-            </div>
+            <Menu/>
+            
+            <Feed />
+          
         </div>
     )
 }
