96894dbce6bd1ce0dfe2d998e48c03b25083cb45
Ludmylla <ludmay4@gmail.com>
2022-12-13 19:54:09 -0300

    adiciona route ao menuItem

--- frontend/parrot/src/components/MenuItem/index.tsx
+++ frontend/parrot/src/components/MenuItem/index.tsx
@@ -1,19 +1,23 @@
 import { ReactNode } from "react";
+import { Link } from "react-router-dom";
 import { Slot } from "@radix-ui/react-slot";
 import Text from "../Text";
 
 interface MenuItemProps {
     menuTitle: string;
     children?: ReactNode;
+    route: string;
 }
 
 function MenuItem(props: MenuItemProps) {
     return (
         <li className="mt-5">
+            <Link to={props.route}>
             <div className="flex items-center px-4 rounded-full text-gray-500 hover:bg-sky-400 ml-2">
                 <Slot className="text-slate-50">{props.children}</Slot>
                 <Text className="font-extrabold ml-4">{props.menuTitle}</Text>
             </div>
+            </Link>
         </li>
     )
 }
