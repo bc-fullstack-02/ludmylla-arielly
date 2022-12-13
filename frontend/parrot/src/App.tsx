05e8c405d5101ae36aa6fc42a78493aeb775f6b5
Ludmylla <ludmay4@gmail.com>
2022-12-13 19:52:54 -0300

    adiciona rota para friends e profile page

--- frontend/parrot/src/App.tsx
+++ frontend/parrot/src/App.tsx
@@ -2,12 +2,16 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
+import ProfilePage from './Pages/ProfilePage'
 import './index.css'
+import Friends from './Pages/Friends'
 
 const router = createBrowserRouter([
   { path:'/', element: <Login /> },
   { path:'/signup', element: <SignUp /> },
-  { path: '/home', element: <Home /> }
+  { path: '/home', element: <Home /> },
+  { path: '/profile', element: <ProfilePage />},
+  { path: '/friends', element: <Friends />}
 ])
 
 function App() {
