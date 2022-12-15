3deac00279cce3c4a1668e4906d678d1a19aa6d0
Ludmylla <ludmay4@gmail.com>
2022-12-15 11:32:47 -0300

    upload image profile

---
+++ backend/social-network/api/lib/upload-profile.js
@@ -0,0 +1,47 @@
+const multer = require('multer')
+
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+
+const upload = multer({ storage: multer.memoryStorage() })
+const bucketName = 'first-bucket'
+
+const config = {
+  region: 'us-east-1',
+  endpoint: process.env.BUCKET_ENDPOINT || 'http://minio:9000/',
+  forcePathStyle: true,
+  sslEnabled: false,
+  signatureVersion: 'v4',
+  credentials: {
+    accessKeyId: process.env.BUCKET_ACCESS_KEY || 'myminiouser',
+    secretAccessKey: process.env.BUCKET_SECRET_KEY || 'myminiopassword'
+  }
+
+}
+
+const bucketHost = 'http://localhost:9000/'
+const s3Client = new S3Client(config)
+
+module.exports = [
+  upload.single('file'), (req, res, next) => {
+    if (req.file) {
+      const filename = `${req.user.profile.id}/${req.file.originalname}`
+      return s3Client
+        .send(
+          new PutObjectCommand({
+            Bucket: bucketName,
+            Key: filename,
+            ContentType: req.file.mimetype,
+            Body: req.file.buffer
+          })
+        )
+        .then(() => {
+          req.body.image = true
+          req.body.imageProfile = `${bucketHost || config.endpoint}${bucketName}/${filename}`
+          return next()
+        })
+        .catch(next)
+    } else {
+      next()
+    }
+  }
+]
