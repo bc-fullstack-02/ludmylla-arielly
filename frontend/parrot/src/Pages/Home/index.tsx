3a4225e3a90a57b276b649807f68b0a26c41e365
Ludmylla <ludmay4@gmail.com>
2022-12-21 09:54:15 -0300

    refatorando like e unlike

--- frontend/parrot/src/Pages/Home/index.tsx
+++ frontend/parrot/src/Pages/Home/index.tsx
@@ -10,41 +10,71 @@ import { Post } from '../../model/post';
 function Home() {
 
     const authHeader = getAuthHeader()
-    const profile = localStorage.getItem('profile') as string;
+    const profile = localStorage.getItem('profile') as never;
     const user = localStorage.getItem('user') as string;
 
     const [posts, setPosts] = useState<Post[]>([]);
 
-
     useEffect(() => {
         async function getPosts() {
-            const response = await api.get('/feed',  authHeader); 
-
+            const response = await api.get('/feed', authHeader);
             setPosts(response.data);
         }
         getPosts();
     }, []);
 
-    async function handleLike(postId: String) {
-       try {
+    async function likePost(postId: String) {
+        try {
             await api.post(`/posts/${postId}/like`, null, authHeader);
             const newPost = posts.filter((post) => post._id === postId)
-            .map((post) => {
-                post.likes.push(profile)
-                return post;
-            });
+                .map((post) => {
+                    post.likes.push(profile)
+                    return post;
+                });
 
-           setPosts((posts) => {
-            const post = newPost[0]
+            changePostItem(newPost[0])
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async function unlikePost(postId: String) {
+        try {
+            await api.post(`/posts/${postId}/like`, null, authHeader);
+            const newPost = posts.filter((post) => post._id === postId)
+                .map((post) => {
+                    const index = post.likes.indexOf(profile);
+                    post.likes.splice(index, 1);
+                    return post;
+                });
+
+            changePostItem(newPost[0])
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    function changePostItem(newPost: Post) {
+        setPosts((posts) => {
+            const post = newPost;
             const index = posts.indexOf(post)
             posts[index] = post
-            return [ ...posts]
-           })
+            return [...posts]
+        })
+    }
 
-       }catch(err) {
-            console.error(err);
-       }
-        
+    async function handleLike(postId: String) {
+        const likePosts = posts.filter(post => post._id === postId)
+            .map(post => post.likes.includes(profile));
+
+        if (likePosts && !likePosts[0]) {
+            likePost(postId)
+            console.log(likePost)
+        } else {
+            unlikePost(postId);
+        }
     }
 
     async function newPostCreated(post: Post) {
@@ -52,18 +82,17 @@ function Home() {
             const response = await api.get(`/posts/${post._id}`, authHeader)
             const newPost = response.data;
             setPosts((posts) => [newPost, ...posts]);
-        } catch(err) {
+        } catch (err) {
             console.error(err);
         }
-     
     }
 
     return (
         <div className="w-screen h-screen flex">
-            <Menu newPostCreated={newPostCreated}/>
-            
-            <Feed posts={posts} handleLike={handleLike}/>
-          
+            <Menu newPostCreated={newPostCreated} />
+
+            <Feed posts={posts} handleLike={handleLike} />
+
         </div>
     )
 }
