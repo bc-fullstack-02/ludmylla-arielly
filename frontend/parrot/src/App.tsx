cdcb1361646fd48baa0b73f7afee2c2a5a36c99c
Ludmylla <ludmay4@gmail.com>
2022-12-23 12:38:40 -0300

    cria rota para postDetails

--- frontend/parrot/src/App.tsx
+++ frontend/parrot/src/App.tsx
@@ -1,17 +1,21 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
 import ProfilePage from './Pages/ProfilePage'
-import './index.css'
 import Friends from './Pages/Friends'
+import PostDetails from './Pages/PostDetails'
+
+import './index.css'
 
 const router = createBrowserRouter([
   { path:'/', element: <Login /> },
   { path:'/signup', element: <SignUp /> },
   { path: '/home', element: <Home /> },
   { path: '/profile', element: <ProfilePage />},
-  { path: '/friends', element: <Friends />}
+  { path: '/friends', element: <Friends />},
+  {path: '/posts/:postId', element: <PostDetails />}
 ])
 
 function App() {
