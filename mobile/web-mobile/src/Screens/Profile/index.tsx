432a59f0331d79df87777f065c3ee23d6c5c86d9
Ludmylla <ludmay4@gmail.com>
2022-12-30 19:37:50 -0300

    adiciona focusAwareStatusBar ao porfile

--- mobile/web-mobile/src/Screens/Profile/index.tsx
+++ mobile/web-mobile/src/Screens/Profile/index.tsx
@@ -1,24 +1,30 @@
 import React, { useContext } from 'react';
 import { UserCircle } from 'phosphor-react-native';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Button from '../../components/Button';
+import { FocusAwareStatusBar } from '../../components/FocusAwareStatusBar';
 
 import { Context as AuthContext } from '../../content/AuthContent';
 
+import { THEME } from '../../Theme';
 import { styles } from './style';
-
 
 export function Profile() {
   const { user, logout } = useContext(AuthContext);
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <FocusAwareStatusBar
+           barStyle='light-content'
+           backgroundColor={THEME.COLORS.BACKGROUND_800}
+        />
        <View style={styles.heading}>
         <UserCircle color='white' size={48} weight='thin' />
         <Text style={styles.userNameText}>{user}</Text>
       </View>
       <Button onPress={logout} title='Sair' />
-    </View>
+    </SafeAreaView>
   )
 }
