5331ae374dc444cec56a7fd4f889bc65e6d0a436
Ludmylla <ludmay4@gmail.com>
2022-12-30 19:33:51 -0300

    cria FocusAwareStatusBar

---
+++ mobile/web-mobile/src/components/FocusAwareStatusBar/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { useIsFocused } from "@react-navigation/native";
+import { StatusBar, StatusBarProps } from 'react-native';
+
+interface Props extends StatusBarProps{}
+
+export function FocusAwareStatusBar(props: Props) {
+    const isFocused = useIsFocused();
+
+    return isFocused ? <StatusBar {...props}/> : null;
+}
