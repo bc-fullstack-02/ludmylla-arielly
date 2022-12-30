a63f9fdd15bbad9a031dfd2d7b2970ffead270e8
Ludmylla <ludmay4@gmail.com>
2022-12-30 20:45:33 -0300

    adiciona like e unlike ao postItem

--- mobile/web-mobile/src/components/PostItem/index.tsx
+++ mobile/web-mobile/src/components/PostItem/index.tsx
@@ -1,6 +1,9 @@
-import React from "react";
-import { Image, Text, View } from "react-native";
+import React, {useContext} from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { Chat, Heart, UserCircle } from "phosphor-react-native";
+
+import { Context as PostContext } from "../../content/PostContent";
+import { Context as AuthContext } from "../../content/AuthContent";
 
 import { Post } from "../../@types/post";
 
@@ -10,8 +13,18 @@ interface PostItemProps {
     post: Post,
 }
 export function PostItem({ post }: PostItemProps) {
-    return (
+    const {likePost, unlike} = useContext(PostContext);
+    const {profile} = useContext(AuthContext);
 
+    function handleLikePress() {
+        if (post.likes.includes(profile)) {
+            unlike && unlike({postId: post._id})
+        }else {
+            likePost && likePost({postId: post._id})
+        }
+    }
+
+    return (
         <View style={styles.post}>
             <View style={styles.postHeading}>
                 <UserCircle color='white' size={48} weight='thin' />
@@ -28,9 +41,15 @@ export function PostItem({ post }: PostItemProps) {
     
             <View style={styles.footer}>
                 <Chat size={24} color='white' weight='thin' />
-                <Text style={styles.number}>{post.likes.length}</Text>
-                <Heart size={24} color='white' weight='thin' />
                 <Text style={styles.number}>{post.comments.length}</Text>
+                <TouchableOpacity onPress={handleLikePress}>
+                   {post.likes.includes(profile) ? (
+                     <Heart size={24} color='red' weight='fill' />
+                   ) : (
+                     <Heart size={24} color='white' weight='thin' />
+                   )}
+                </TouchableOpacity>
+                <Text style={styles.number}>{post.likes.length}</Text>
             </View>
         </View>
 
